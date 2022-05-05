ea6e8d9dfb6f4ccc6c043165fb57464ac06c12b1
mila9821 <91022954+mila9821@users.noreply.github.com>
2022-05-05 08:14:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/navigationcomponentmila/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.navigationcomponentmila;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private  static final long SPLASH_SCREEN_DELAY = 5000;
+
+    Animation topAnim, bottomAnim;
+    ImageView image, texto;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Animacion
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.anima_logo);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.anima_texto);
+
+        //Logica y disenio
+        image = findViewById(R.id.idlogo);
+        texto = findViewById(R.id.idtexto);
+
+        image.setAnimation(topAnim);
+        texto.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, inicioFragment.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN_DELAY);
+
+
+
+    }
+}
